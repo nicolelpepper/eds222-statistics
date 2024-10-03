f80856570d7535c480eff4bea049a80fdedead2e
nicolelpepper <nicolelpepper@gmail.com>
2024-10-03 10:53:46 -0700

    Initial commit

---
+++ eds222-discussions/week1-discussion.R
@@ -0,0 +1,17 @@
+
+
+
+# load librarys
+library(tidyverse)
+library(gt)
+library(openintro)
+library(here)
+
+
+# load librarys
+source(here::here("eds222-labs","_common.R"))
+
+
+# for labs we want to see all our code.
+knitr::opts_chunk$set(echo = TRUE)
+
